5c3a30e178d4e6bb62ae1a7dc1e89e69adc87b55
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-10-12 21:22:35 -0600

    update user.js module.export

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 //import the sequelize constructor from the library
-const PORT = process.env.PORT || 3001;
-const Sequalize = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 // create connction to our db
-const sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
